6f2577c507d8d08369fdcb7f03ac1f62073baa68
Danie12345 <danielmaloosorio@gmail.com>
2019-11-13 00:41:16 -0600

    Update de archivo objects.py
    Se cambió lo que hace la clase Buildings; ahora tiene un initializer, y un método para producir.

--- objects.py
+++ objects.py
@@ -1,2 +1,7 @@
 class Buildings:
-	pass
+	def __init__(self, base=1, production=1):
+		self.produce = base
+		self.production = production
+	
+	def Produce(self):
+		return self.produce*self.production
